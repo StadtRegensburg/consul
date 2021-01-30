c93e98c4beffa0684ee1c340cb8e33bbd333dd55
itw-smart <tobias@demokratie.today>
2021-01-30 13:31:05 +0100

    code clean

--- app/controllers/custom/proposals_controller.rb
+++ app/controllers/custom/proposals_controller.rb
@@ -15,23 +15,24 @@ class ProposalsController
   end
 
   private
-  def process_tags
-    params[:proposal][:tag_list_categories].split(",").each do |t|
-      next if t.strip.blank?
-      Tag.find_or_create_by name: t.strip, kind: :category
+    def process_tags
+      params[:proposal][:tag_list_categories].split(",").each do |t|
+        next if t.strip.blank?
+        Tag.find_or_create_by name: t.strip, kind: :category
+      end
+      params[:proposal][:tag_list_subcategories].split(",").each do |t|
+        next if t.strip.blank?
+        Tag.find_or_create_by name: t.strip, kind: :subcategory
+      end
+      params[:proposal][:tag_list] ||= ""
+      params[:proposal][:tag_list] += ((params[:proposal][:tag_list_categories] || "").split(",") + (params[:proposal][:tag_list_subcategories] || "").split(",")).join(",")
+      params[:proposal][:tag_list_categories], params[:proposal][:tag_list_subcategories] = nil, nil
     end
-    params[:proposal][:tag_list_subcategories].split(",").each do |t|
-      next if t.strip.blank?
-      Tag.find_or_create_by name: t.strip, kind: :subcategory
+    
+    def take_only_by_tag_names
+      if params[:tags].present?
+        @resources = @resources.tagged_with(params[:tags].split(","), all: true)
+        @subcategories = @resources.tag_counts.subcategory
+      end
     end
-    params[:proposal][:tag_list] ||= ""
-    params[:proposal][:tag_list] += ((params[:proposal][:tag_list_categories] || "").split(",") + (params[:proposal][:tag_list_subcategories] || "").split(",")).join(",")
-    params[:proposal][:tag_list_categories], params[:proposal][:tag_list_subcategories] = nil, nil
-  end
-  def take_only_by_tag_names
-    if params[:tags].present?
-      @resources = @resources.tagged_with(params[:tags].split(","), all: true)
-      @subcategories = @resources.tag_counts.subcategory
-    end
-  end
 end
