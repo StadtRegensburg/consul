794ef960e372ad234d7b4ed6f97a3f040baf6076
itw-smart <tobias@demokratie.today>
2021-01-30 13:09:24 +0100

    code clean

--- app/controllers/custom/proposals_controller.rb
+++ app/controllers/custom/proposals_controller.rb
@@ -4,6 +4,7 @@ class ProposalsController
   
   before_action :process_tags, only: [:create, :update]
   
+  
   def index_customization
     discard_draft
     discard_archived
@@ -14,27 +15,27 @@ class ProposalsController
     take_only_by_tag_names
   end
 
-private
+  private
 
-  def process_tags
-    params[:proposal][:tag_list_categories].split(",").each do |t|
-      next if t.strip.blank?
-      Tag.find_or_create_by name: t.strip, kind: :category
+    def process_tags
+      params[:proposal][:tag_list_categories].split(",").each do |t|
+        next if t.strip.blank?
+        Tag.find_or_create_by name: t.strip, kind: :category
+      end
+      params[:proposal][:tag_list_subcategories].split(",").each do |t|
+        next if t.strip.blank?
+        Tag.find_or_create_by name: t.strip, kind: :subcategory
+      end
+      params[:proposal][:tag_list] ||= ""
+      params[:proposal][:tag_list] += ((params[:proposal][:tag_list_categories] || "").split(",") + (params[:proposal][:tag_list_subcategories] || "").split(",")).join(",")
+      params[:proposal][:tag_list_categories], params[:proposal][:tag_list_subcategories] = nil, nil
     end
-    params[:proposal][:tag_list_subcategories].split(",").each do |t|
-      next if t.strip.blank?
-      Tag.find_or_create_by name: t.strip, kind: :subcategory
-    end
-    params[:proposal][:tag_list] ||= ""
-    params[:proposal][:tag_list] += ((params[:proposal][:tag_list_categories] || "").split(",") + (params[:proposal][:tag_list_subcategories] || "").split(",")).join(",")
-    params[:proposal][:tag_list_categories], params[:proposal][:tag_list_subcategories] = nil, nil
-  end
 
-  def take_only_by_tag_names
-    if params[:tags].present?
-      @resources = @resources.tagged_with(params[:tags].split(","), all: true)
-      @subcategories = @resources.tag_counts.subcategory
+    def take_only_by_tag_names
+      if params[:tags].present?
+        @resources = @resources.tagged_with(params[:tags].split(","), all: true)
+        @subcategories = @resources.tag_counts.subcategory
+      end
     end
-  end
   
 end
