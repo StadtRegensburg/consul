339f08e9160315c7929219ec4bce140e848786dd
itw-smart <tobias@demokratie.today>
2021-01-30 12:52:52 +0100

    code clean

--- app/controllers/custom/proposals_controller.rb
+++ app/controllers/custom/proposals_controller.rb
@@ -1,6 +1,7 @@
 require_dependency Rails.root.join("app", "controllers", "proposals_controller").to_s
 
 class ProposalsController
+  
   before_action :process_tags, only: [:create, :update]
   
   def index_customization
@@ -14,6 +15,7 @@ class ProposalsController
   end
 
 private
+
   def process_tags
     params[:proposal][:tag_list_categories].split(",").each do |t|
       next if t.strip.blank?
@@ -27,10 +29,12 @@ private
     params[:proposal][:tag_list] += ((params[:proposal][:tag_list_categories] || "").split(",") + (params[:proposal][:tag_list_subcategories] || "").split(",")).join(",")
     params[:proposal][:tag_list_categories], params[:proposal][:tag_list_subcategories] = nil, nil
   end
+
   def take_only_by_tag_names
     if params[:tags].present?
       @resources = @resources.tagged_with(params[:tags].split(","), all: true)
       @subcategories = @resources.tag_counts.subcategory
     end
   end
+  
 end
