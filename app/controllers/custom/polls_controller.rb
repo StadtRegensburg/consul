9c09eb880f250e0a0cf654bccc9049c9bb8034fd
itw-smart <tobias@demokratie.today>
2021-01-31 08:18:44 +0100

    code clean

--- app/controllers/custom/polls_controller.rb
+++ app/controllers/custom/polls_controller.rb
@@ -32,7 +32,7 @@ class PollsController < ApplicationController
 
     def take_only_by_tag_names
       if params[:tags].present?
-        @resources = @resources.tagged_with(params[:tags].split(","), all: true)
+        @resources = @resources.tagged_with(params[:tags].split(","), all: true, any: :true)
         @subcategories = @resources.tag_counts.subcategory
       end
     end
--- app/controllers/custom/proposals_controller.rb
+++ app/controllers/custom/proposals_controller.rb
@@ -31,7 +31,7 @@ class ProposalsController
     
     def take_only_by_tag_names
       if params[:tags].present?
-        @resources = @resources.tagged_with(params[:tags].split(","), all: true)
+        @resources = @resources.tagged_with(params[:tags].split(","), all: true, any: :true)
         @subcategories = @resources.tag_counts.subcategory
       end
     end
