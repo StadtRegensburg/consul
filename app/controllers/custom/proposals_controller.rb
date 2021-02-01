2bd6f8b33580c1bc987219751dd486902bb65458
itw-smart <tobias@demokratie.today>
2021-02-01 13:34:18 +0100

    code clean

--- app/controllers/custom/proposals_controller.rb
+++ app/controllers/custom/proposals_controller.rb
@@ -13,30 +13,6 @@ class ProposalsController
     load_featured
     remove_archived_from_order_links
     take_only_by_tag_names
-    @proposals_coordinates = all_proposal_map_locations
-  end
-  
-  def all_proposal_map_locations
-    ids = if params[:search]
-      Proposal.search(params[:search]).pluck(:id)
-    elsif params[:tags]
-      Proposal.not_archived.published.tagged_with(params[:tags].split(","), all: true)
-    else
-      Proposal.not_archived.published.pluck(:id)
-    end
-      MapLocation.where(proposal_id: ids).map(&:json_data)
-  end
-
-  def json_data
-    proposal = Proposal.find(params[:id])
-    data = {
-      proposal_id: proposal.id,
-      proposal_title: proposal.title
-    }.to_json
-
-    respond_to do |format|
-      format.json { render json: data }
-    end
   end
 
   private
