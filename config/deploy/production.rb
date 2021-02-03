45bcd40964ea9762e337ea0e27861d0ade2ceba5
itw-smart <tobias@itw-smartbusiness.de>
2021-02-03 10:57:23 +0100

    fonts

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :branch, ENV["branch"] || :master
+set :branch, ENV["branch"] || :cli_auf
 
 server deploysecret(:server1), user: deploysecret(:user), roles: %w[web app db importer cron background]
 #server deploysecret(:server2), user: deploysecret(:user), roles: %w(web app db importer cron background)
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
-set :branch, ENV["branch"] || :master
+set :branch, ENV["branch"] || :cli_auf
 
 
 server deploysecret(:server), user: deploysecret(:user), roles: %w[web app db importer cron background]
