edce19cfb68ecd418c34934d517dd06ad69f0fe7
itw-smart <tobias@itw-smartbusiness.de>
2021-02-08 12:52:33 +0100

    code clean

---
+++ db/migrate/20210203202811_add_tags_logic_for_tags.rb
@@ -0,0 +1,6 @@
+class AddTagsLogicForTags < ActiveRecord::Migration[5.1]
+  def change
+    add_column :tags, :custom_logic_category_code, :integer, default: 0
+    add_column :tags, :custom_logic_subcategory_code, :integer, default: 0
+  end
+end
