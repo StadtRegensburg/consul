1df948a844b5d3c88d2ef26e2fca2f4b5e739286
itw-smart <tobias@itw-smartbusiness.de>
2021-02-08 15:44:55 +0100

    code clean

--- app/models/custom/proposal.rb
+++ app/models/custom/proposal.rb
@@ -1,7 +1,7 @@
 require_dependency Rails.root.join("app", "models", "proposal").to_s
 class Proposal < ApplicationRecord
-  MANAGE_CATEGORIES    = 0b010
-  MANAGE_SUBCATEGORIES = 0b100
+  MANAGE_CATEGORIES    = 0b011
+  MANAGE_SUBCATEGORIES = 0b010
 
   TAGS_PREDEFINED = 0b001
   TAGS_CLOUD      = 0b010
