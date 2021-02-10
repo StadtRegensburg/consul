2c2648a2118229f091e931c03cc01fbedec1af25
itw-smart <tobias@itw-smartbusiness.de>
2021-02-10 16:46:45 +0100

    code clean

--- app/models/custom/debate.rb
+++ app/models/custom/debate.rb
@@ -1,7 +1,7 @@
 require_dependency Rails.root.join("app", "models", "debate").to_s
 class Debate < ApplicationRecord
-  MANAGE_CATEGORIES    = 0b110
-  MANAGE_SUBCATEGORIES = 0b110
+  MANAGE_CATEGORIES    = 0b010
+  MANAGE_SUBCATEGORIES = 0b010
 
   TAGS_PREDEFINED = 0b001
   TAGS_CLOUD      = 0b010
