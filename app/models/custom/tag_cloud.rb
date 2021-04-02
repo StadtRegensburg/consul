8c06020275a31ad49b0b14d2bfe17fd5f86abdef
Mike Svystun <michael.svystun@gmail.com>
2021-04-02 15:12:01 +0300

    bug with custom tags

--- app/models/custom/tag_cloud.rb
+++ app/models/custom/tag_cloud.rb
@@ -1,4 +1,11 @@
 require_dependency Rails.root.join("app", "models", "tag_cloud").to_s
 
 class TagCloud
+  def tags
+    resource_model.
+    last_week.tag_counts.
+    where("lower(name) NOT IN (?)", category_names + geozone_names + default_blacklist).
+    order("#{table_name}_count": :desc, name: :asc).
+    limit(10)
+  end
 end
