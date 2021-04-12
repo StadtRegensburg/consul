dc6f979c0d7d77142236a68f99187ad19374d5b9
Mike Svystun <michael.svystun@gmail.com>
2021-04-12 16:49:54 +0300

    con-261

--- app/assets/javascripts/custom/click_out_warning.js
+++ app/assets/javascripts/custom/click_out_warning.js
@@ -2,8 +2,19 @@
   "use strict";
   App.ClickOutWarning = {
     initialize: function() {
-      $("body").on("click", "a", function() {
-        alert('test')
+      $("body").on("click", "a", function(event) {
+        if (document.querySelector("meta[name='external-links']").getAttribute("content") === 'active' ) {
+          var showWarning = true;
+        }
+
+        if ( showWarning && this.hostname.length && location.hostname !== this.hostname ) {
+
+          var answer = window.confirm("Mit Bestätigung stimmen Sie zu, die Webseite der Stadt zu verlassen.");
+          if ( !answer ) {
+            event.preventDefault()
+          }
+
+        }
       });
     }
   }
