f6135fea7f27826a52d1c206cf001adb7dabe2dc
Mike Svystun <michael.svystun@gmail.com>
2021-05-05 12:24:38 +0300

    con-316

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -5,7 +5,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 
 CKEDITOR.editorConfig = function( config )
 {
-  config.extraPlugins = 'widget,lineutils,mjAccordion,youtube';
+  config.extraPlugins = 'widget,lineutils,mjAccordion,youtube,div,iframe';
   config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
   config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
   config.filebrowserImageUploadUrl = "/ckeditor/pictures?";
