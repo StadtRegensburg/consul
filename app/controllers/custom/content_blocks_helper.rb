302dd239631c3b7bc4d955f1592c6fa05525451a
Mike Svystun <michael.svystun@gmail.com>
2021-05-18 22:20:20 +0300

    con-380

--- app/controllers/custom/content_blocks_helper.rb
+++ app/controllers/custom/content_blocks_helper.rb
@@ -4,7 +4,7 @@ module ContentBlocksHelper
   def render_custom_block(key)
     block = SiteCustomization::ContentBlock.custom_block_for(key, I18n.locale)
     if current_user && current_user.administrator?
-      edit_link = link_to t("admin.actions.edit"), edit_admin_site_customization_content_block_path(block, return_to: request.path )
+      edit_link = link_to '', edit_admin_site_customization_content_block_path(block, return_to: request.path), class: 'edit-custom-block-link'
     end
     res = block&.body || ""
     if edit_link
