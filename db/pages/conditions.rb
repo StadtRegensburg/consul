7ac2e4e781becb00561e39c71d35eecbf775e418
Mike Svystun <michael.svystun@gmail.com>
2021-05-26 17:25:04 +0300

    con-376

--- db/pages/conditions.rb
+++ db/pages/conditions.rb
@@ -1,7 +1,70 @@
+def conditions_page_content
+  content = "<p>#{I18n.t("pages.conditions.description")}</p>"
+
+
+  content += '<p><strong>1. Allgemeines</strong></p>'
+
+  content += '<p>1.1 Die Stadt <span class="replace-highlighted">xyz, Straße, PLZ Ort („Betreiberin“)</span>, betreibt die Online-Plattform <span class="replace-highlighted">www.xyz.de („Plattform“)</span>. Die Plattform ermöglicht es <span class="replace-highlighted">xyz</span> („Nutzer“), sich über <span class="replace-highlighted">xyz</span> zu informieren und sich an <span class="replace-highlighted">xyz</span> zu beteiligen.</p>'
+  
+  content += '<p>1.2 Für die Nutzungsbeziehung zwischen der Betreiberin und dem Nutzer gelten ausschließlich die nachfolgenden Nutzungsbedingungen in ihrer zum Zeitpunkt des Vertragsschlusses gültigen Fassung. Abweichende Nutzungsbedingungen des Nutzers werden nicht anerkannt, es sei denn, die Betreiberin stimmt der Geltung der abweichenden Nutzungsbedingungen des Nutzers ausdrücklich schriftlich zu.</p>'
+
+
+  content += '<p>2. Vertragsschluss, Vertragsdauer und Kündigung</p>'
+
+  content += '<p>2.1 Die kostenfreie Nutzung der Plattform erfolgt auf Grundlage eines Nutzungsvertrags. Dieser Nutzungsvertrag kommt durch die kostenfreie Registrierung des Nutzers (Angebot) und Bestätigung der Registrierung durch die Betreiberin (Annahme des Angebots) zustande. Die Laufzeit des Nutzungsvertrages ist unbefristet. </p>'
+
+  content += '<p>2.2 Der Nutzer ist verpflichtet, bei der Registrierung vollständige und richtige Angaben zu machen und diese bei Bedarf zu aktualisieren. Der Nutzer ist weiterhin verpflichtet, die Zugangsdaten zu der Plattform sicher zu verwahren. Der Nutzer darf keinen weiteres Benutzerkonto erstellen, seine Zugangsdaten nicht an Dritte weitergeben und darf auch Dritten nicht den Zugang zu der Plattform über seine Zugangsdaten ermöglichen.</p>'
+
+  content += '<p>2.3 Der Nutzungsvertrag kann vom Nutzer und von der Betreiberin jederzeit ordentlich durch einfache Mitteilung an die Betreiberin gekündigt werden. Der Nutzer kann den Nutzungsvertrag jederzeit ordentlich auch dadurch kündigen, indem er sein Benutzerkonto löscht. Das Recht zur außerordentlichen Kündigung aus wichtigem Grund bleibt unberührt. Für die Betreiberin liegt ein wichtiger Grund insbesondere dann vor, wenn der Nutzer seine Verpflichtungen nach Ziffer 3 oder 4 dieses Nutzungsvertrages nachhaltig verletzt.</p>'
+
+
+  content += '<p><strong>3. Nutzung der Plattform</strong></p>'
+
+  content += '<p>3.1 Die Betreiberin bietet dem Nutzer mittels der Plattform die Möglichkeit sich über <span class="replace-highlighted">xyz</span> zu informieren und sich an <span class="replace-highlighted">xyz</span> zu beteiligen, etwa indem er <span class="replace-highlighted">xyz</span>. Weiterhin kann der Nutzer <span class="replace-highlighted">xyz</span>. An <span class="replace-highlighted">xyz</span>, die nur für <span class="replace-highlighted">xyz</span> bestimmt sind, können sich nur derjenige Nutzer beteiligen, der dort seinen ersten Wohnsitz hat.</p>'
+
+  content += '<p>3.2 Die Betreiberin hat das Recht, die Plattform jederzeit hinsichtlich Funktionsweise, Gestaltung, Inhalt und anderer Merkmale zu ändern. Die Betreiberin hat weiterhin das Recht, den Zugang zu der Plattform aufgrund von Wartungsarbeiten, Kapazitätsbelangen und anderer Ereignisse ganz oder teilweise, zeitweise oder auf Dauer, einzuschränken. Der Nutzer erkennt an, dass technisch bedingt die Plattform nicht  immer verfügbar sein könnte.</p>'
+
+  content += '<p>3.3 Hat die Betreiberin konkrete Anhaltspunkte dafür, dass ein Dritter das Benutzerkonto eines Nutzers unbefugt nutzt, oder dafür, dass ein Nutzer gegen diese Nutzungsbedingungen verstößt, ist die Betreiberin berechtigt, das Benutzerkonto dieses Nutzers zeitweise oder vollständig zu sperren.</p>'
+
+
+  content += '<p>4. Pflichten des Nutzers</p>'
+
+  content += '<p>4.1 Der Nutzer darf die Plattform nur für private Zwecke nutzen.</p>'
+
+  content += '<p>4.2 Der Nutzer verpflichtet sich, bei der Erstellung und Verwendung eigener Inhalte geltendes Recht (z.B. Straf- und Jugendschutzrecht) einzuhalten und keine Rechte Dritter (z.B. Namens-, Marken-, Urheber- und Datenschutzrechte) zu verletzen. Der Nutzer verpflichtet sich insbesondere, keine Inhalte auf der Plattform zu veröffentlichen, die beleidigend, diskriminierend, rassistisch, religiöse Gefühle verletzend, extremistisch, diffamierend, sexuell belästigend, pornografisch oder anstößig sind.</p>'
+
+  content += '<p>4.3 Der Nutzer verpflichtet sich jede Handlung zu unterlassen, die geeignet ist, die Funktionalität der Plattform zu beeinträchtigen, insbesondere die Plattform übermäßig zu belasten. Der Nutzer verpflichtet sich weiterhin, jede Handlung unterlassen, die geeignet ist, technische Beschränkungen und Zugangsbeschränkungen der Plattform zu umgehen, und/oder die Plattform zu verändern, zu modifizieren, weiterzuentwickeln oder weiterzuverbreiten.</p>'
+
+
+  content += '<p><strong>5. Haftung</strong></p>'
+
+  content += '<p>5.1 Die Nutzung der Plattform erfolgt freiwillig.</p>'
+
+  content += '<p>5.2 Der Nutzer ist alleine für seine Inhalte und für die Einhaltung der gesetzlichen und sonstigen Vorschriften und Regelungen verantwortlich. Die Betreiberin kann nicht für die Inhalte des Nutzers verantwortlich gemacht werden.</p>'
+
+  content += '<p>5.3 Der Nutzer stellt die Betreiberin hiermit von allen Ansprüchen frei, die Dritte wegen einer Verletzung ihrer Rechte durch Inhalte des Nutzers gegen die Betreiberin geltend machen.</p>'
+
+  content += '<p>5.4 Die Betreiberin ist berechtigt, die Inhalte eines Nutzers vorläufig oder dauerhaft zu sperren, vom Netz zu nehmen, oder sie so zu verändern, dass sie Rechte Dritter nicht mehr verletzen oder die geforderten Unterlassungserklärungen abzugeben, wenn die Betreiberin wegen des Inhalts eines Nutzers von Dritten auf Unterlassung in Anspruch genommen wird, oder wenn die Betreiberin sonst von der Rechteverletzung Kenntnis erlangt.</p>'
+
+  content += '<p>5.5 Die Betreiberin haftet für die Plattform vorbehaltlich der nachstehenden Regelungen nach den gesetzlichen Vorschriften.</p>'
+
+  content += '<p>5.6 Für alle gegen die Betreiberin gerichteten Ansprüche auf Schadens- und Aufwendungsersatz wegen zu vertretender Pflichtverletzung haftet die Betreiberin im Falle leichter Fahrlässigkeit nur bei einer den Vertragszweck gefährdenden Verletzung wesentlicher Pflichten. Im Übrigen ist die Haftung der Betreiberin für leichte Fahrlässigkeit ausgeschlossen. Im Falle einer Haftung nach dem Vorstehenden und einer Haftung ohne Verschulden haftet die Betreiberin nur für den typischen und vorhersehbaren Schaden.</p>'
+
+  content += '<p>5.7 Die vorstehende Haftungsbeschränkung gilt nicht bei einer Gefährdungshaftung und für Schäden aufgrund der Verletzung des Lebens, des Körpers oder der Gesundheit.</p>'
+
+  content += '<p>6. Schlussbestimmungen</p>'
+
+  content += '<p>6.1 Auf diese Nutzungsbedingungen findet ausschließlich deutsches Recht Anwendung.</p>'
+
+  content += '<p>6.2 Die Betreiberin behält sich vor, diese Nutzungsbedingungen jederzeit zu ändern und die weitere Nutzung der Plattform von der Zustimmung zu den geänderten Nutzungsbedingungen abhängig zu machen.</p>'
+
+  content += '<p>6.3 Sollten eine oder mehrere Bestimmungen dieser Nutzungsbedingungen unwirksam sein oder unwirksam werden, wird dadurch die Wirksamkeit der anderen Bestimmungen nicht berührt.</p>'
+end
+
 def generate_content(page)
   page.title = I18n.t("pages.conditions.title")
   page.subtitle = I18n.t("pages.conditions.subtitle")
-  page.content = "<p>#{I18n.t("pages.conditions.description")}</p>"
+  page.content = conditions_page_content
   page.save!
 end
 
