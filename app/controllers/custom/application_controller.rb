839fdef4eab8089370c3c6ba361aed7e0a8749ba
Mike Svystun <michael.svystun@gmail.com>
2021-06-08 13:57:35 +0300

    con-396

--- app/controllers/custom/application_controller.rb
+++ app/controllers/custom/application_controller.rb
@@ -3,7 +3,7 @@ require_dependency Rails.root.join("app", "controllers", "application_controller
 
 class ApplicationController < ActionController::Base
 
-  before_action :set_active_and_archived_projekts
+  before_action :set_active_and_archived_projekts, :set_default_social_media_images
 
   private
 
@@ -18,5 +18,12 @@ class ApplicationController < ActionController::Base
   def set_active_and_archived_projekts
     @active_projekts = Projekt.top_level_active.joins(:projekt_settings).where("projekt_settings.key = 'projekt_feature.show_in_navigation' AND projekt_settings.value = 'active'").distinct
     @archived_projekts = Projekt.top_level_archived
+  end
+
+  def set_default_social_media_images
+    social_media_icon_path = SiteCustomization::Image.all.find_by(name: 'social_media_icon').image.url.split('?')[0]
+    @social_media_icon_path = social_media_icon_path.include?('missing') ? nil : social_media_icon_path
+    social_media_icon_twitter_path = SiteCustomization::Image.all.find_by(name: 'social_media_icon_twitter').image.url.split('?')[0]
+    @social_media_icon_twitter_path = social_media_icon_twitter_path.include?('missing') ? nil : social_media_icon_twitter_path
   end
 end
