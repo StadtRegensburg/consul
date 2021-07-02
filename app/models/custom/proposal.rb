c608c56d9cdaac9135f536c01d7d5f6e2fd173f8
Mike Svystun <michael.svystun@gmail.com>
2021-07-02 11:22:07 +0300

    bug

--- app/models/custom/proposal.rb
+++ app/models/custom/proposal.rb
@@ -30,8 +30,7 @@ class Proposal < ApplicationRecord
   end
 
   def description_sanitized
-    sanitized_description = ActionController::Base.helpers.strip_tags(description).gsub("\n", '').gsub("\r", '').gsub(" ", '').gsub(/^$\n/, '')
-    byebug
+    sanitized_description = ActionController::Base.helpers.strip_tags(description).gsub("\n", '').gsub("\r", '').gsub(" ", '').gsub(/^$\n/, '').gsub(/[\u202F\u00A0\u2000\u2001\u2003]/, "")
     errors.add(:description, :too_long, message: 'too long text') if
       sanitized_description.length > Setting[ "extended_option.proposals.description_max_length"].to_i
   end
