5b681238ccadf30d62b0360a504ed433dc0c54ac
Mike Svystun <michael.svystun@gmail.com>
2021-08-19 11:20:11 +0300

    update selectable_by for projekt_phase

--- app/models/custom/projekt_phase.rb
+++ app/models/custom/projekt_phase.rb
@@ -4,21 +4,17 @@ class ProjektPhase < ApplicationRecord
   has_many :geozone_restrictions, through: :projekt_phase_geozones, source: :geozone
 
   def selectable_by?(user)
-    geozone_allowed = if geozone_restricted && geozone_restriction_ids.any?
-                        geozone_restriction_ids.include?(user.geozone_id) || Setting["feature.user.skip_verification"].present?
-                      elsif geozone_restricted
-                        user.level_two_or_three_verified? || Setting["feature.user.skip_verification"].present?
-                      else 
-                        true
-                      end
+    geozone_allowed = geozone_restricted == "no_restriction" ||
+                      geozone_restricted == "only_citizens" ||
+                      ( geozone_restricted == "only_geozones" && geozone_restrictions.blank? ) ||
+                      ( geozone_restricted == "only_geozones" && geozone_restrictions.any? && user.present? && geozone_restrictions.include?(user.geozone) )
 
-    user.present? &&
-      geozone_allowed &&
-         self.active &&
-           ((self.start_date <= Date.today if self.start_date) || self.start_date.blank? ) &&
-             ((self.end_date >= Date.today if self.end_date) || self.end_date.blank? )
-
-
+    user &&
+      user.level_three_verified? &&
+        geozone_allowed &&
+          self.active &&
+            ((self.start_date <= Date.today if self.start_date) || self.start_date.blank? ) &&
+              ((self.end_date >= Date.today if self.end_date) || self.end_date.blank? )
   end
 
   def expired?
