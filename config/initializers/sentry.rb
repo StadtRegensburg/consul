d35824c268659d1fb6a37c6b675d75501e6f0cbb
Mike Svystun <michael.svystun@gmail.com>
2021-09-13 13:54:27 +0300

    minor bug

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-  # config.dsn = 'https://ae87de89cb154acc9e4b3b77f35efb96@o994111.ingest.sentry.io/5952564'
+  config.dsn = Rails.application.secrets.sentry_key
   config.breadcrumbs_logger = [:active_support_logger]
 
   # To activate performance monitoring, set one of these options.
