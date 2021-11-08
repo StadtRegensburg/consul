3365fe6695d7dd10b9bb8c69397195141ef55462
Mike Svystun <michael.svystun@gmail.com>
2021-11-08 11:04:33 +0200

    minor changes

--- lib/wysiwyg_sanitizer.rb
+++ lib/wysiwyg_sanitizer.rb
@@ -1,18 +1,16 @@
 class WYSIWYGSanitizer
   def allowed_tags
-    %w[
-      p ul ol li strong em u s a h2 h3 h4 h5 h6 div span img iframe br
-      input
-    ]
+    %w[ p ul ol li strong em u s a h1 h2 h3 h4 h5 h6 div span img iframe br i ]
   end
 
   def allowed_attributes
     %w[
-      href style class id target onclick width height
+      href style class id target width height
       data-slider data-initial-start data-end data-slider-handle role tabindex data-slider-fill type
       data-dropdown-menu
       data-drilldown
       data-accordion-menu
+      allowfullscreen
     ]
   end
 
