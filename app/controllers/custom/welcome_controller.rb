8ba2ed266f6eac80d028adf477d4124a86b77198
Mike Svystun <michael.svystun@gmail.com>
2021-11-16 19:03:46 +0200

    part 2

--- app/controllers/custom/welcome_controller.rb
+++ app/controllers/custom/welcome_controller.rb
@@ -14,5 +14,14 @@ class WelcomeController < ApplicationController
     @remote_translations = detect_remote_translations(@feeds,
                                                       @recommended_debates,
                                                       @recommended_proposals)
+
+    @latest_polls = Poll.current.order(created_at: :asc).limit(3)
+
+
+    latest_proposals = Proposal.last_week
+    latest_debates = Debate.last_week
+    @latest_resources = (latest_proposals.to_a + latest_debates.to_a).sort_by(&:created_at).reverse.first(3)
+    set_debate_votes(latest_debates)
+    set_proposal_votes(latest_proposals)
   end
 end
