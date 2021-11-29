0dfffcd5bdf095ffca19b084fc7846f0fcc56e8d
Mike Svystun <michael.svystun@gmail.com>
2021-11-29 10:57:59 +0200

    new form

---
+++ app/components/deficiency_reports/new_component.rb
@@ -0,0 +1,12 @@
+class DeficiencyReports::NewComponent < ApplicationComponent
+  include Header
+  attr_reader :deficiency_report
+
+  def initialize(deficiency_report)
+    @deficiency_report = deficiency_report
+  end
+
+  def title
+    t("custom.deficiency_report.new.start_new")
+  end
+end
