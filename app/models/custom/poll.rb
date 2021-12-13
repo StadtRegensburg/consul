9bbca6a536ca808d0fd8ee3cf0fb84c28c6ecb52
Mike Svystun <michael.svystun@gmail.com>
2021-12-13 12:15:51 +0200

    bug with removing last vote

--- app/models/custom/poll.rb
+++ app/models/custom/poll.rb
@@ -31,10 +31,10 @@ class Poll < ApplicationRecord
     voters.count == 0
   end
 
-  def delete_voter_participation_if_no_votes(token)
-    poll_answer_count_by_current_user = questions.inject(0) { |sum, question| sum + question.answers.where(author: author).count }
+  def delete_voter_participation_if_no_votes(user, token)
+    poll_answer_count_by_current_user = questions.inject(0) { |sum, question| sum + question.answers.where(author: user).count }
     if poll_answer_count_by_current_user == 0
-      Poll::Voter.find_by!(user: author, poll: self, origin: "web", token: token).destroy
+      Poll::Voter.find_by!(user: user, poll: self, origin: "web", token: token).destroy
     end
   end
 end
