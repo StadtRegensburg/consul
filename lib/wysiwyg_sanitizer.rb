72185e3d9d5518fa37b32724ed64607be4ea4c50
Mike Svystun <michael.svystun@gmail.com>
2021-12-21 15:20:26 +0200

    con-805

--- lib/wysiwyg_sanitizer.rb
+++ lib/wysiwyg_sanitizer.rb
@@ -1,13 +1,13 @@
 class WYSIWYGSanitizer
   def allowed_tags
     %w[ div p ul ol li blockquote br hr a h2 h3 h4 h5 h6 strong em u s sub sup span img
-    table caption thead tr th tbody td
+    table caption thead tr th tbody td abbr
   ]
   end
 
   def allowed_attributes
     %w[
-      href style target class id name alt src align border cellpadding cellspacing summary scope
+      href style target class id name alt src align border cellpadding cellspacing summary scope title
     ]
   end
 
