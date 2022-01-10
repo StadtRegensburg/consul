9f521be81936ce4bd3810189a5e44f9322ec261d
Mike Svystun <michael.svystun@gmail.com>
2022-01-10 14:32:01 +0200

    con-820

--- app/controllers/custom/admin/projekt_settings_controller.rb
+++ app/controllers/custom/admin/projekt_settings_controller.rb
@@ -1,21 +1,24 @@
 class Admin::ProjektSettingsController < Admin::BaseController
   def update
-    @projekt = Projekt.find(params[:projekt_id])
-    @projekt_setting = ProjektSetting.find_by(id: params[:id], projekt: @projekt)
-    @projekt_setting.update!(projekt_settings_params)
-    redirect_to request_referer, notice: t("admin.settings.flash.updated")
+    @projekt_setting = ProjektSetting.find_by(id: params[:projekt_id]) #workaround to avoid editing FeaturedSettingsComponent
+    @projekt_setting.update!(projekt_setting_params)
+
+    respond_to do |format|
+      format.html { redirect_to request_referer, notice: t("admin.settings.flash.updated") }
+      format.js
+    end
   end
 
   def update_default_projekt_footer_tab
     default_tab_setting = ProjektSetting.find_by(id: params[:id])
-    default_tab_setting.update(projekt_settings_params)
+    default_tab_setting.update(projekt_setting_params)
 
     redirect_to request_referer, notice: t("admin.settings.flash.updated")
   end
 
   private
 
-  def projekt_settings_params
+  def projekt_setting_params
     params.require(:projekt_setting).permit(:value)
   end
 
