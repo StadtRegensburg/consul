e927d8664f8a5c8b01fa0c39089e998db8d3fea9
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2022-02-16 18:16:25 +0200

    con-895

--- app/controllers/custom/admin/deficiency_reports_controller.rb
+++ app/controllers/custom/admin/deficiency_reports_controller.rb
@@ -1,11 +1,13 @@
 class Admin::DeficiencyReportsController < Admin::BaseController
+  include Translatable
+
   def index
-    super
+    @deficiency_reports = DeficiencyReport.all.order(id: :asc).page((params[:page].presence || 0)).per(params[:limit].presence || 20)
 
     respond_to do |format|
       format.html
       format.csv do
-        send_data DeficiencyReport::CsvExporter.new(@resources).to_csv,
+        send_data DeficiencyReport::CsvExporter.new(@deficiency_reports).to_csv,
           filename: "deficiency_reports.csv"
       end
     end
