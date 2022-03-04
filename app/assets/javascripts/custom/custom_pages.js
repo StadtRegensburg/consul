b8b1e62db4a45fd021433efe4f971b3c32fe4a4a
Mike Svystun <michael.svystun@gmail.com>
2022-03-05 01:08:24 +0200

    con-784

--- app/assets/javascripts/custom/custom_pages.js
+++ app/assets/javascripts/custom/custom_pages.js
@@ -1,39 +1,6 @@
 (function() {
   "use strict";
   App.CustomPages = {
-    scrollTabsLeft: function() {
-      var $tabToShow = $('#filter-subnav li.page-subnav-tab:visible').first().prev()
-      var $tabToHide = $('#filter-subnav li.page-subnav-tab:visible').last()
-      $tabToShow.show();
-      $tabToHide.hide();
-
-      var $firstTab = $('#filter-subnav li.page-subnav-tab').first()
-      if ( $firstTab.is(':hidden') ) {
-        $('#left-arrow-control').show();
-      } else {
-        $('#left-arrow-control').hide();
-      }
-
-      $('#right-arrow-control').show();
-    },
-
-    scrollTabsRight: function() {
-      var $tabToShow = $('#filter-subnav li.page-subnav-tab:visible').last().next()
-      var $tabToHide = $('#filter-subnav li.page-subnav-tab:visible').first()
-      $tabToShow.show();
-      $tabToHide.hide();
-
-      var $firstTab = $('#filter-subnav li.page-subnav-tab').first()
-      if ( $firstTab.is(':hidden') ) {
-        $('#left-arrow-control').show();
-      }
-
-      var $lastTab = $('#filter-subnav li.page-subnav-tab').last()
-      if ( $lastTab.is(':visible') ) {
-        $('#right-arrow-control').hide();
-      }
-    },
-
     initialize: function() {
       $("body").on("click", ".js-icon-toggle-budget-phases", function(event) {
         var $phase = $(this).closest('.sidebar-projekt-phase')
@@ -45,19 +12,17 @@
       })
 
       $("body").on("click", ".js-left-arrow-control", function(event) {
-        var $firstTab = $('#filter-subnav li.page-subnav-tab').first()
-        if ( $firstTab.css('display') == 'none' ) {
-          App.CustomPages.scrollTabsLeft();
-        }
+        $('#filter-subnav').animate( { scrollLeft: '-=120' }, 1000 );
       })
 
       $("body").on("click", ".js-right-arrow-control", function(event) {
-        var $lastTab = $('#filter-subnav li.page-subnav-tab').last()
+        $('#filter-subnav').animate( { scrollLeft: '+=120' }, 1000 );
 
-        if ( $lastTab.css('display') == 'none' ) {
-          App.CustomPages.scrollTabsRight();
-        }
+        // var element = document.getElementById('filter-subnav')
+        // var maxScroll = element.scrollWidth - element.clientWidth
       })
+
+
     }
   }
 }).call(this);
