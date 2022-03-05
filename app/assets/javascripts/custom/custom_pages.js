8126f26b65c7979c2dd51e7a12337551756200b6
Mike Svystun <michael.svystun@gmail.com>
2022-03-05 14:35:59 +0200

    con-784

--- app/assets/javascripts/custom/custom_pages.js
+++ app/assets/javascripts/custom/custom_pages.js
@@ -13,13 +13,33 @@
 
       $("body").on("click", ".js-left-arrow-control", function(event) {
         $('#filter-subnav').animate( { scrollLeft: '-=120' }, 1000 );
+
+        var tabFilterSubnav = document.getElementById('filter-subnav')
+        var maxScroll = tabFilterSubnav.scrollWidth - tabFilterSubnav.clientWidth
+
+        if ( $('#filter-subnav').scrollLeft() <= 120 ) {
+          $('#left-arrow-control').addClass('disabled')
+        }
+
+        if ( $('#filter-subnav').scrollLeft() != maxScroll) {
+          $('#right-arrow-control').removeClass('disabled')
+        }
       })
 
       $("body").on("click", ".js-right-arrow-control", function(event) {
         $('#filter-subnav').animate( { scrollLeft: '+=120' }, 1000 );
 
-        // var element = document.getElementById('filter-subnav')
-        // var maxScroll = element.scrollWidth - element.clientWidth
+        var tabFilterSubnav = document.getElementById('filter-subnav')
+        var maxScroll = tabFilterSubnav.scrollWidth - tabFilterSubnav.clientWidth
+
+        if ( maxScroll != 0 ) {
+          $('#left-arrow-control').removeClass('disabled')
+        }
+
+        if ( maxScroll <= $('#filter-subnav').scrollLeft() + 120 ) {
+          $('#right-arrow-control').addClass('disabled')
+        }
+
       })
 
 
