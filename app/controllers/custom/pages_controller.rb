5ef393ea49a6d814ad0c4a8a787df742c2a49c1b
Mike Svystun <michael.svystun@gmail.com>
2022-03-06 15:41:59 +0200

    con-784

--- app/controllers/custom/pages_controller.rb
+++ app/controllers/custom/pages_controller.rb
@@ -6,10 +6,6 @@ class PagesController < ApplicationController
   include CustomHelper
   include ProposalsHelper
 
-  has_orders %w[most_voted newest oldest], only: [ :show, :comment_phase_footer_tab ]
-  has_orders ->(c) { Proposal.proposals_orders(c.current_user) }, only: :proposal_phase_footer_tab
-  has_orders ->(c) { Debate.debates_orders(c.current_user) }, only: :debate_phase_footer_tab
-  has_filters %w[all current], only: :voting_phase_footer_tab
 
   def show
     @custom_page = SiteCustomization::Page.published.find_by(slug: params[:id])
@@ -18,7 +14,8 @@ class PagesController < ApplicationController
 
     if @custom_page.present? && @custom_page.projekt.present?
       @projekt = @custom_page.projekt
-      @proposals_coordinates = all_projekt_proposals_map_locations(@custom_page.projekt)
+      @first_phase_name = @projekt.projekt_phases.order(:start_date).first.resources_name
+      send("set_#{@first_phase_name}_footer_tab_variables", @projekt)
 
       scoped_projekt_ids = @projekt.all_children_projekts.unshift(@projekt).pluck(:id)
       @comments_count = @projekt.comments.count
@@ -26,26 +23,7 @@ class PagesController < ApplicationController
       @proposals_count = Proposal.base_selection(scoped_projekt_ids).count
       @polls_count = Poll.base_selection(scoped_projekt_ids).count
 
-      @most_active_proposals = Proposal.published.not_archived.where(projekt: @custom_page.projekt).sort_by_hot_score.limit(3)
-      set_proposal_votes(@most_active_proposals)
-
-      @most_active_debates = Debate.where(projekt: @custom_page.projekt).sort_by_hot_score.limit(3)
-      set_debate_votes(@most_active_debates)
-
-      @latest_polls = Poll.where(projekt: @custom_page.projekt.all_children_ids.push(@custom_page.projekt.id)).order(created_at: :asc).limit(3)
-
       @cards = @custom_page.cards
-
-      @geozones = Geozone.all
-
-      @selected_geozone_affiliation = params[:geozone_affiliation] || 'all_resources'
-      @affiliated_geozones = (params[:affiliated_geozones] || '').split(',').map(&:to_i)
-
-      @selected_geozone_restriction = params[:geozone_restriction] || 'no_restriction'
-      @restricted_geozones = (params[:restricted_geozones] || '').split(',').map(&:to_i)
-
-      @rss_id = ProjektSetting.find_by(projekt: @custom_page.projekt, key: "projekt_newsfeed.id").value
-      @rss_type = ProjektSetting.find_by(projekt: @custom_page.projekt, key: "projekt_newsfeed.type").value
 
       render action: :custom_page
     elsif @custom_page.present?
@@ -59,12 +37,7 @@ class PagesController < ApplicationController
   end
 
   def comment_phase_footer_tab
-    @current_projekt = Projekt.find(params[:id])
-    @current_tab_phase = @current_projekt.comment_phase
-
-    @commentable = @current_projekt
-    @comment_tree = CommentTree.new(@commentable, params[:page], @current_order)
-    set_comment_flags(@comment_tree.comments)
+    set_comments_footer_tab_variables
 
     respond_to do |format|
       format.js { render "pages/projekt_footer/footer_tab" }
@@ -72,24 +45,7 @@ class PagesController < ApplicationController
   end
 
   def debate_phase_footer_tab
-    @current_projekt = Projekt.find(params[:id])
-    @current_tab_phase = @current_projekt.debate_phase
-
-    @selected_parent_projekt = @current_projekt
-
-    scoped_projekt_ids = @current_projekt.all_children_projekts.unshift(@current_projekt).pluck(:id)
-
-    @current_order = params[:order] || @valid_orders.first
-    @valid_orders.delete("archival_date")
-
-    @all_resources = Debate.base_selection(scoped_projekt_ids)
-
-    take_by_projekts
-    set_top_level_projekts
-
-    set_debate_votes(@all_resources)
-
-    @debates = @all_resources.page(params[:page]).send("sort_by_#{@current_order}")
+    set_debates_footer_tab_variables
 
     respond_to do |format|
       format.js { render "pages/projekt_footer/footer_tab" }
@@ -97,26 +53,7 @@ class PagesController < ApplicationController
   end
 
   def proposal_phase_footer_tab
-    @current_projekt = Projekt.find(params[:id])
-    @current_tab_phase = @current_projekt.proposal_phase
-
-    @selected_parent_projekt = @current_projekt
-
-    scoped_projekt_ids = @current_projekt.all_children_projekts.unshift(@current_projekt).pluck(:id)
-
-    @current_order = params[:order] || @valid_orders.first
-    @valid_orders.delete("archival_date")
-
-    @all_resources = Proposal.base_selection(scoped_projekt_ids)
-
-    take_by_projekts
-    set_top_level_projekts
-
-    set_proposal_votes(@all_resources)
-
-    @proposals_coordinates = all_proposal_map_locations(@all_resources)
-
-    @proposals = @all_resources.page(params[:page]).send("sort_by_#{@current_order}")
+    set_proposals_footer_tab_variables
 
     respond_to do |format|
       format.js { render "pages/projekt_footer/footer_tab" }
@@ -124,18 +61,7 @@ class PagesController < ApplicationController
   end
 
   def voting_phase_footer_tab
-    @current_projekt = Projekt.find(params[:id])
-    @current_tab_phase = @current_projekt.voting_phase
-    @selected_parent_projekt = @current_projekt
-
-    scoped_projekt_ids = @current_projekt.all_children_projekts.unshift(@current_projekt).pluck(:id)
-
-    @all_resources = Poll.base_selection(scoped_projekt_ids).send(@current_filter)
-
-    take_by_projekts
-    set_top_level_projekts
-
-    @polls = Kaminari.paginate_array(@all_resources.sort_for_list).page(params[:page])
+    set_polls_footer_tab_variables
 
     respond_to do |format|
       format.js { render "pages/projekt_footer/footer_tab" }
@@ -143,29 +69,7 @@ class PagesController < ApplicationController
   end
 
   def budget_phase_footer_tab
-    @current_projekt = Projekt.find(params[:id])
-    @current_tab_phase = @current_projekt.budget_phase
-
-    params[:filter_projekt_id] ||= params[:id]
-
-    @budget = Budget.find_by(projekt_id: params[:filter_projekt_id])
-
-    query = Budget::Ballot.where(user: current_user, budget: @budget)
-    @ballot = @budget.balloting? ? query.first_or_create! : query.first_or_initialize
-
-    @investments = @budget.investments
-    @investment_ids = @investments.ids
-
-    if @budget.present? && @current_projekt.current?
-      @top_level_active_projekts = Projekt.where( id: @current_projekt )
-      @top_level_archived_projekts = []
-    elsif @budget.present? && @current_projekt.expired?
-      @top_level_active_projekts = []
-      @top_level_archived_projekts = Projekt.where( id: @current_projekt )
-    else
-      @top_level_active_projekts = []
-      @top_level_archived_projekts = []
-    end
+    set_budget_footer_tab_variables
 
     respond_to do |format|
       format.js { render "pages/projekt_footer/footer_tab" }
@@ -191,5 +95,107 @@ class PagesController < ApplicationController
   def set_top_level_projekts
     @top_level_active_projekts = Projekt.where( id: @current_projekt ).selectable_in_sidebar_current(@current_tab_phase.resources_name)
     @top_level_archived_projekts = Projekt.where( id: @current_projekt ).selectable_in_sidebar_expired(@current_tab_phase.resources_name)
+  end
+
+  def set_comments_footer_tab_variables(projekt=nil)
+    @valid_orders = %w[most_voted newest oldest]
+    @current_order = @valid_orders.include?(params[:order]) ? params[:order] : @valid_orders.first
+
+    @current_projekt = projekt || Projekt.find(params[:id])
+    @current_tab_phase = @current_projekt.comment_phase
+
+    @commentable = @current_projekt
+    @comment_tree = CommentTree.new(@commentable, params[:page], @current_order)
+    set_comment_flags(@comment_tree.comments)
+  end
+
+  def set_debates_footer_tab_variables(projekt=nil)
+    @valid_orders = Debate.debates_orders(current_user)
+    @valid_orders.delete('relevance')
+    @current_order = @valid_orders.include?(params[:order]) ? params[:order] : @valid_orders.first
+
+    @current_projekt = projekt || Projekt.find(params[:id])
+    @current_tab_phase = @current_projekt.debate_phase
+
+    @selected_parent_projekt = @current_projekt
+
+    scoped_projekt_ids = @current_projekt.all_children_projekts.unshift(@current_projekt).pluck(:id)
+
+    @all_resources = Debate.base_selection(scoped_projekt_ids)
+
+    take_by_projekts
+    set_top_level_projekts
+
+    set_debate_votes(@all_resources)
+
+    @debates = @all_resources.page(params[:page]).send("sort_by_#{@current_order}")
+  end
+
+  def set_proposals_footer_tab_variables(projekt=nil)
+    @valid_orders = Proposal.proposals_orders(current_user)
+    @valid_orders.delete('relevance')
+    @current_order = @valid_orders.include?(params[:order]) ? params[:order] : @valid_orders.first
+
+    @current_projekt = projekt || Projekt.find(params[:id])
+    @current_tab_phase = @current_projekt.proposal_phase
+
+    @selected_parent_projekt = @current_projekt
+
+    scoped_projekt_ids = @current_projekt.all_children_projekts.unshift(@current_projekt).pluck(:id)
+
+    @all_resources = Proposal.base_selection(scoped_projekt_ids)
+
+    take_by_projekts
+    set_top_level_projekts
+
+    set_proposal_votes(@all_resources)
+
+    @proposals_coordinates = all_proposal_map_locations(@all_resources)
+
+    @proposals = @all_resources.page(params[:page]).send("sort_by_#{@current_order}")
+  end
+
+  def set_polls_footer_tab_variables(projekt=nil)
+    @valid_filters = %w[all current]
+    @current_filter = @valid_filters.include?(params[:filter]) ? params[:filter] : @valid_filters.first
+
+    @current_projekt = projekt || Projekt.find(params[:id])
+    @current_tab_phase = @current_projekt.voting_phase
+    @selected_parent_projekt = @current_projekt
+
+    scoped_projekt_ids = @current_projekt.all_children_projekts.unshift(@current_projekt).pluck(:id)
+
+    @all_resources = Poll.base_selection(scoped_projekt_ids).send(@current_filter)
+
+    take_by_projekts
+    set_top_level_projekts
+
+    @polls = Kaminari.paginate_array(@all_resources.sort_for_list).page(params[:page])
+  end
+
+  def set_budget_footer_tab_variables(projekt=nil)
+    @current_projekt = projekt || Projekt.find(params[:id])
+    @current_tab_phase = @current_projekt.budget_phase
+
+    params[:filter_projekt_id] ||= params[:id]
+
+    @budget = Budget.find_by(projekt_id: params[:filter_projekt_id])
+
+    query = Budget::Ballot.where(user: current_user, budget: @budget)
+    @ballot = @budget.balloting? ? query.first_or_create! : query.first_or_initialize
+
+    @investments = @budget.investments
+    @investment_ids = @investments.ids
+
+    if @budget.present? && @current_projekt.current?
+      @top_level_active_projekts = Projekt.where( id: @current_projekt )
+      @top_level_archived_projekts = []
+    elsif @budget.present? && @current_projekt.expired?
+      @top_level_active_projekts = []
+      @top_level_archived_projekts = Projekt.where( id: @current_projekt )
+    else
+      @top_level_active_projekts = []
+      @top_level_archived_projekts = []
+    end
   end
 end
