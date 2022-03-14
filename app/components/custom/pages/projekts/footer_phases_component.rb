754aadf42c8cedef227d9d13874f8f8c5be2f6de
Mike Svystun <michael.svystun@gmail.com>
2022-03-14 18:14:48 +0200

    con-930

--- app/components/custom/pages/projekts/footer_phases_component.rb
+++ app/components/custom/pages/projekts/footer_phases_component.rb
@@ -1,5 +1,5 @@
 class Pages::Projekts::FooterPhasesComponent < ApplicationComponent
-  delegate :format_date_range, :get_projekt_phase_restriction_name, to: :helpers
+  delegate :format_date, :format_date_range, :get_projekt_phase_restriction_name, to: :helpers
   attr_reader :projekt, :default_phase_name, :phases, :milestone_phase
 
   def initialize(projekt, default_phase_name)
