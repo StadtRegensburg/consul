635eefa7423750ebbde05f8304c866a5cf10aad0
Mike Svystun <michael.svystun@gmail.com>
2022-03-29 14:21:55 +0300

    con-956

--- app/components/custom/shared/order_links_component.rb
+++ app/components/custom/shared/order_links_component.rb
@@ -5,7 +5,7 @@ class Shared::OrderLinksComponent < ApplicationComponent
   private
     def link_path(order)
       if params[:current_tab_path]
-        url_for(action: params[:current_tab_path], controller: 'pages', order: order, page: 1, anchor: anchor)
+        url_for(action: params[:current_tab_path], controller: 'pages', order: order, page: 1, anchor: anchor, filter_projekt_ids: params[:filter_projekt_ids])
       else
         current_path_with_query_params(order: order, page: 1, anchor: anchor)
       end
