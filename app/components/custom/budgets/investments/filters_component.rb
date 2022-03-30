e6b4a3eaa12fe211c284d1583d44f1ef29fe99b0
Mike Svystun <michael.svystun@gmail.com>
2022-03-30 10:28:16 +0300

    con-962

---
+++ app/components/custom/budgets/investments/filters_component.rb
@@ -0,0 +1,28 @@
+require_dependency Rails.root.join("app", "components", "budgets", "investments", "filters_component").to_s
+
+class Budgets::Investments::FiltersComponent < ApplicationComponent
+  private
+
+    def filters
+      valid_filters.map do |filter|
+        [
+          t("budgets.investments.index.filters.#{filter}"),
+          link_path(filter),
+          current_filter == filter,
+          remote: remote?
+        ]
+      end
+    end
+
+    def link_path(filter)
+      if params[:current_tab_path] == 'budget_phase_footer_tab'
+        url_for(action: params[:current_tab_path], controller: 'pages', page: 1, filter: filter, filter_projekt_ids: params[:filter_projekt_ids])
+      else
+        current_path_with_query_params(filter: filter, page: 1)
+      end
+    end
+
+    def remote?
+      controller_name == 'pages'
+    end
+end
--- app/controllers/custom/pages_controller.rb
+++ app/controllers/custom/pages_controller.rb
@@ -220,6 +220,8 @@ class PagesController < ApplicationController
   def set_budget_footer_tab_variables(projekt=nil)
     params[:filter_projekt_id] = projekt&.id || SiteCustomization::Page.find_by(slug: params[:id]).projekt.id
     @current_projekt = Projekt.find(params[:filter_projekt_id])
+    @valid_filters = @current_projekt.budget.investments_filters
+
     @current_tab_phase = @current_projekt.budget_phase
     params[:current_tab_path] = 'budget_phase_footer_tab'
 
@@ -231,6 +233,8 @@ class PagesController < ApplicationController
     @ballot = @budget.balloting? ? query.first_or_create! : query.first_or_initialize
 
     @investments = @budget.investments
+    @investments = @investments.send(params[:filter]) if params[:filter]
+
     @investment_ids = @investments.ids
 
     if @budget.present? && @current_projekt.current?
