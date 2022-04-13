447d7a6442c20686b177dfb9c0f514cc0bb7366c
Mike Svystun <michael.svystun@gmail.com>
2022-04-13 17:41:09 +0300

    con-1011

--- app/models/custom/budget.rb
+++ app/models/custom/budget.rb
@@ -3,4 +3,14 @@ require_dependency Rails.root.join("app", "models", "budget").to_s
 class Budget < ApplicationRecord
   belongs_to :projekt, optional: true
   has_one :budget_phase, through: :projekt
+
+  def investments_filters
+    [
+      ("winners" if finished?),
+      ("selected" if publishing_prices_or_later? && !finished?),
+      ("unselected" if publishing_prices_or_later?),
+      ("not_unfeasible" if selecting? || valuating?),
+      ("unfeasible" if selecting? || valuating_or_later?)
+    ].compact
+  end
 end
