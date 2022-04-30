52e5c5b34600994bf8ec6ee499b62a9eccca253e
Mike Svystun <michael.svystun@gmail.com>
2022-04-30 20:04:53 +0300

    con-1052

--- app/components/custom/deficiency_reports/comments_form_component.rb
+++ app/components/custom/deficiency_reports/comments_form_component.rb
@@ -1,5 +1,5 @@
 class DeficiencyReports::CommentsFormComponent < ApplicationComponent
-  delegate :current_user, :user_signed_in?, to: :helpers
+  delegate :current_user, :user_signed_in?, :link_to_verify_account, to: :helpers
 
   def initialize(deficiency_report)
     @deficiency_report = deficiency_report
