2d42fb3ad63bdc4d7f0343fc101b179982bc5385
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2022-05-13 21:36:32 +0300

    con-1067

--- app/assets/javascripts/custom/projekt_question.js
+++ app/assets/javascripts/custom/projekt_question.js
@@ -1,29 +1,58 @@
 (function() {
   "use strict";
   App.ProjektQuestionCustom = {
-    loadNextQuestion: function(href) {
-      $.get(href).then(function(response) {
-        $('.js-projekt-question-section').replaceWith(response)
-      })
+    debounce: function(func, duration, immediate) {
+      var timeout;
+
+      return function() {
+        var context = this, args = arguments;
+        var later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+
+        clearTimeout(timeout);
+        timeout = setTimeout(later, duration);
+
+        if (callNow) func.apply(context, args);
+      };
+    },
+
+    // reloadCurrentQuestion: function() {
+    //   var href = document.querySelector('.js-project-question-list').dataset.questionUrl
+    //
+    //   this.loadQuestion(href)
+    // },
+
+    loadQuestion: function(url) {
+      $.get(url)
+    },
+
+    submitForm: function(e) {
+      var $element = $(e.currentTarget)
+      $element.closest('form').trigger('submit.rails')
+      var $elementLabel = $element.parent('label')
+
+      $elementLabel.parent().find('label input[type="radio"]').prop('disabled', true)
+
+      $elementLabel.siblings('label').removeClass('is-active')
+      $elementLabel.addClass('is-active')
+      // $(".js-projekt-question-section").addClass("show-loader");
     },
 
     initialize: function() {
       $("body").on("click", ".js-projekt-question-next", function(e) {
         e.preventDefault()
         e.stopPropagation()
-        this.loadNextQuestion(e.currentTarget.href)
+        this.loadQuestion(e.currentTarget.href)
       }.bind(this));
 
-      $('body').on('change', '.js-project-question-list form input', function(e) {
-        var $element = $(e.currentTarget)
+      // $('body').on('ajax:success', '.js-projekt-answer-form', function(data) {
+        // this.reloadCurrentQuestion()
+      // }.bind(this));
 
-        $element.closest('form').trigger('submit.rails')
-
-        var $elementLabel = $element.parent('label')
-
-        $elementLabel.siblings('label').removeClass('is-active')
-        $elementLabel.addClass('is-active')
-      })
+      $('body').on('change', '.js-project-question-list form input', this.debounce(this.submitForm.bind(this), 500))
     }
   }
 }).call(this);
