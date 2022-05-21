8dad678b9bb39bd5909ee9afe3b6a1e8f41a629e
Mike Svystun <michael.svystun@gmail.com>
2022-05-21 17:35:52 +0300

    minor bug

--- app/models/custom/milestone.rb
+++ app/models/custom/milestone.rb
@@ -1,7 +1,8 @@
 require_dependency Rails.root.join("app", "models", "milestone").to_s
 
 class Milestone < ApplicationRecord
-  def self.order_by_publication_date(order = :asc)
-    order(publication_date: order, created_at: :asc)
+  def self.order_by_publication_date(sorting_order = nil)
+    sorting_order ||= :asc
+    order(publication_date: sorting_order, created_at: :asc)
   end
 end
