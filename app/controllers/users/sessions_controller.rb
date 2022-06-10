2468c1c60b963bdf33706102672b1acf6b32509e
Mike Svystun <michael.svystun@gmail.com>
2022-06-10 14:38:31 +0300

    debug

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
   def destroy
+    logger.tagged('sessions#destroy') { logger.info "stored_location_for(:user): #{stored_location_for(:user)}" } 
     @stored_location = stored_location_for(:user)
     super
   end
@@ -8,13 +9,17 @@ class Users::SessionsController < Devise::SessionsController
 
     def after_sign_in_path_for(resource)
       if !verifying_via_email? && resource.show_welcome_screen?
+        logger.tagged('after_sign_in_path_for') { logger.info "Welcome path: #{welcome_path}" } 
         welcome_path
       else
+        logger.tagged('after_sign_in_path_for') { logger.info "Super path: #{super}" } 
         super
       end
     end
 
     def after_sign_out_path_for(resource)
+      logger.tagged('after_sign_out_path_for') { logger.info "@stored_location: #{@stored_location}" } 
+      logger.tagged('after_sign_out_path_for') { logger.info "Super: #{super}" } 
       @stored_location.present? && !@stored_location.match("management") ? @stored_location : super
     end
 
