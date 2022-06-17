6bdb9a84b58fd68fb1c53871fae81ab7e978859b
Mike Svystun <michael.svystun@gmail.com>
2022-06-17 18:55:30 +0300

    con-1177

--- db/content_blocks.rb
+++ db/content_blocks.rb
@@ -5,5 +5,6 @@ load Rails.root.join("db", "content_blocks", "deficiency_reports_recommendations
 load Rails.root.join("db", "content_blocks", "proposals_help.rb")
 load Rails.root.join("db", "content_blocks", "debates_help.rb")
 load Rails.root.join("db", "content_blocks", "deficiency_reports_help.rb")
-
 load Rails.root.join("db", "content_blocks", "polls_help.rb")
+
+load Rails.root.join("db", "content_blocks", "admin_site_customization_information_texts_intro.rb")
