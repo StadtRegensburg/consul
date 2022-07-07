096cd2998da72f1049313b7bb646878b5a90cc81
Mike Svystun <michael.svystun@gmail.com>
2022-07-07 13:58:38 +0300

    con-623

--- app/controllers/custom/admin/projekt_events_controller.rb
+++ app/controllers/custom/admin/projekt_events_controller.rb
@@ -1,23 +1,26 @@
 class Admin::ProjektEventsController < Admin::BaseController
-  before_action :set_projekt, except: :destroy
+  before_action :set_projekt
 
   def create
     @projekt_event = ProjektEvent.new(projekt_event_params)
     @projekt_event.projekt = @projekt
     @projekt_event.save
-    redirect_to request_referer, notice: t("admin.settings.flash.updated")
+
+    redirect_to redirect_path(@projekt), notice: t("admin.settings.flash.updated")
   end
 
   def update
     @projekt_event = ProjektEvent.find_by(id: params[:id])
     @projekt_event.update(projekt_event_params)
-    redirect_to request_referer, notice: t("admin.settings.flash.updated")
+
+    redirect_to redirect_path(@projekt), notice: t("admin.settings.flash.updated")
   end
 
   def destroy
     @projekt_event = ProjektEvent.find_by(id: params[:id])
     @projekt_event.destroy
-    redirect_to(request.referer + '#tab-projekt-events')
+
+    redirect_to redirect_path(@projekt)
   end
 
   private
@@ -30,9 +33,11 @@ class Admin::ProjektEventsController < Admin::BaseController
     @projekt = Projekt.find(params[:projekt_id])
   end
 
-  def request_referer
-    return request.referer + params[:projekt_event][:tab] if params[:projekt_event][:tab]
-
-    request.referer
+  def redirect_path(projekt)
+    if params[:role] == 'projekt_manager'
+      edit_projekt_management_projekt_path(projekt) + '#tab-projekt-events'
+    else
+      edit_admin_projekt_path(projekt) + '#tab-projekt-events'
+    end
   end
 end
