896d9a062133f2f38bea1e4ed39d1980f836d6da
Volodymyr Shevchuk <volodymyr.shevchuk.dev@gmail.com>
2022-07-22 11:04:12 +0300

    fix

--- app/components/custom/projekts/projekt_events/list_item.rb
+++ app/components/custom/projekts/projekt_events/list_item.rb
@@ -3,7 +3,8 @@
 class Projekts::ProjektEvents::ListItem < ApplicationComponent
   attr_reader :projekt_event
 
-  def initialize(projekt_event:)
+  def initialize(projekt_event:, show_projekt_link: false)
     @projekt_event = projekt_event
+    @show_projekt_link = show_projekt_link
   end
 end
